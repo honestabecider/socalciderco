992daa082caff6158cd4c127ca7781a539f6931f
Jesus Castellanos <jcastellanos@hugeinc.com>
2018-03-04 17:06:32 -0800

    updated sticky.js to work better all around

--- js/sticky.js
+++ js/sticky.js
@@ -1,18 +1,29 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {handleSticky()};
+window.addEventListener('load', init);
 
-// Get the header
-var header = document.getElementById("container");
-// Get the offset position of the navbar
-var stickyPosition = header.offsetTop;
+
+var header,
+    stickyPostion;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function handleSticky() {
   var windowOffset = window.scrollY;
-
   if (windowOffset >= stickyPosition) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
+}
+
+//resets the stickyPostion variable as the window resizes
+function handleResize() {
+  stickyPosition = header.offsetTop;
+}
+
+function init() {
+  header = document.getElementById("container"); // Get the header
+  stickyPosition = header.offsetTop; // Get the offset position of the navbar
+
+  window.onscroll = function() {handleSticky()};
+  window.addEventListener('resize', handleResize);
 }
