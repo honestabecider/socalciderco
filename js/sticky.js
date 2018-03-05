69a849c572045bc3c4748bc912a3a6a28b8f379e
Jesus Castellanos <jcastellanos@hugeinc.com>
2018-03-04 16:17:09 -0800

    fixed sticky nav by replacing how scroll offset was being set/ calculated

--- js/sticky.js
+++ js/sticky.js
@@ -1,15 +1,16 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {handleSticky()};
 
 // Get the header
 var header = document.getElementById("container");
-
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var stickyPosition = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
+function handleSticky() {
+  var windowOffset = window.scrollY;
+
+  if (windowOffset >= stickyPosition) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
